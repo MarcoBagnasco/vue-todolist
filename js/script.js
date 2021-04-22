b20ab743433f4b03ceb09f65dffb0c2e46a28753
MarcoBagnasco <bagna.hbk@gmail.com>
2021-04-22 15:31:18 +0200

    print todos

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log(Vue);  //test
+// VUE INSTANCE
+const app = new Vue({
+    el: '#root',
+    data: {
+        todos: [
+            {
+                text: 'Fare la spesa',
+                completed: false,
+            },
+            {
+                text: 'Pulire la cantina',
+                completed: false,
+            },
+            {
+                text: 'Preparare la torta',
+                completed: false,
+            },
+        ],
+    },
+});
